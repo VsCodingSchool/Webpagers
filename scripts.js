37e17119fea500bae29265f104255655d75ecb85
VsCodingSchool <144188197+VsCodingSchool@users.noreply.github.com>
2023-09-05 19:55:10 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function myFunc() {
+    document.getElementById("Dems").innerHTML = "func"
+}
+
+
+function myFunc2() {
+   document.getElementById("Nu").innerText = "yeah"
+}
+
+
+function myFunc3() {
+    document.getElementById("Nu").innerText = "U ARE MONKEY"
+ }
